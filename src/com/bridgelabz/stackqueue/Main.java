638f2a9e9914318120352f2bdefe52ad0ad4a66c
altaf <altaf98khan66@gmail.com>
2023-08-05 18:55:04 +0530

    Add: To Peak and pop stack till it is empty.

--- src/com/bridgelabz/stackqueue/Main.java
+++ src/com/bridgelabz/stackqueue/Main.java
@@ -1,11 +1,23 @@
 package com.bridgelabz.stackqueue;
-
 public class Main {
     public static void main(String[] args) {
         Stack stack = new Stack();
         stack.push(70);
         stack.push(30);
         stack.push(56);
+        stack.display();
+
+        //Peek Method:
+        System.out.println("Peek Element is: "+stack.peek());
+
+        //Pop Method:
+        System.out.println("Pop Element is: "+stack.pop());
+        stack.display();
+
+        System.out.println("Pop Element is: "+stack.pop());
+        stack.display();
+
+        System.out.println("Pop Element is: "+stack.pop());
         stack.display();
 
     }
--- src/com/bridgelabz/stackqueue/Stack.java
+++ src/com/bridgelabz/stackqueue/Stack.java
@@ -1,41 +1,65 @@
 package com.bridgelabz.stackqueue;
 
 public class Stack {
-    int[] stack = new int[3];
-    int top = 0;
+    Node top;
+    int size;
+
+    private static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    public boolean empty() {
+        return (top == null);
+    }
 
     public void push(int data) {
-        if (top < stack.length) {
-            stack[top] = data;
-            top++;
-        }
+        Node newNode = new Node(data);
+        newNode.next = top;
+        top = newNode;
+        size++;
     }
 
     public int pop() {
-        if (top > 0) {
-            top--;
-           int data = stack[top];
-            stack[top] = 0;
-            return data;
-        }
-        return -1;
-    }
-
-    /*loop iterates over the elements in the stack array.
-    loop starts from top - 1, which is the index of the top element in the stack,
-    and goes down to 0*/
-    public void display(){
-        for (int i = top - 1; i >= 0; i--) {
-            System.out.println(stack[i]);
+        if (top == null) {
+            System.out.println("Stack is empty. Cannot perform pop operation.");
+            return -1; // Return a default value indicating an empty stack
+        } else {
+            int poppedData = top.data;
+            top = top.next;
+            size--;
+            return poppedData;
         }
     }
-    public int peak(){
-        int data;
-        data = stack[top-1];
-        return data;
+
+    public int size() {
+        return size;
+    }
+    public void display() {
+        if (top == null) {
+            System.out.println("Stack is empty.");
+            return;
+        }
+
+        Node current = top;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+    public int peek() {
+        if (top == null) {
+            System.out.println("Stack is empty.");
+            return -1; // Return a default value indicating an empty stack
+        } else {
+            return top.data;
+        }
     }
 
-    public int getSize(){
-        return top;
-    }
 }
